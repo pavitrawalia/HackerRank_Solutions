a4e1461214619c8413cef2b99c9fbbb861671180
pavitrawalia <30498337+pavitrawalia@users.noreply.github.com>
2019-03-20 09:06:24 +0530

    Create averybigsum.py

---
+++ averybigsum.py
@@ -0,0 +1,26 @@
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the aVeryBigSum function below.
+def aVeryBigSum(ar):
+    sum=0
+    for i in range(0,ar_count):
+        sum+=ar[i]
+    return sum    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(raw_input())
+
+    ar = map(long, raw_input().rstrip().split())
+
+    result = aVeryBigSum(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
